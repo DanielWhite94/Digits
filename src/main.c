fe3f72e5dbe49996bb85c36313462a4910114dec
DJW <asd@example.com>
2020-06-06 16:47:45 +0100

    Updated main demo to test signals

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "digits.h"
+
+DWidgetSignalReturn windowCloseHandler(const DWidgetSignalEvent *event, void *userData);
 
 int main(int argc, char **argv) {
 	// Initialise widget system
@@ -11,11 +14,17 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
-	// Create test window
+	// Create test windows
 	DWidget *window=dWindowNew("My Window", 640, 480);
 
 	DWidget *label=dLabelNew("lolwut noob");
 	dBinAdd(window, label);
+
+	DWidget *window2=dWindowNew("New Win", 1024, 960);
+
+	// Connect signals
+	dWidgetSignalConnect(window, DWidgetSignalTypeWindowClose, &windowCloseHandler, NULL);
+	dWidgetSignalConnect(window2, DWidgetSignalTypeWindowClose, &windowCloseHandler, NULL);
 
 	// Main loop
 	digitsLoop();
@@ -25,4 +34,16 @@ int main(int argc, char **argv) {
 	digitsQuit();
 
 	return 0;
+}
+
+DWidgetSignalReturn windowCloseHandler(const DWidgetSignalEvent *event, void *userData) {
+	assert(event!=NULL);
+	assert(event->type==DWidgetSignalTypeWindowClose);
+	assert(userData==NULL);
+
+	printf("windowCloseHandler for %p - %s\n", event->widget, dWindowGetTitle(event->widget));
+
+	digitsLoopStop();
+
+	return DWidgetSignalReturnContinue;
 }
