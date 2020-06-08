2440d145987123ded3e6f5666170041181df6521
DJW <asd@example.com>
2020-06-08 20:05:22 +0100

    Button now implements logic to generate DWidgetSignalTypeButtonClick signal

--- src/button.c
+++ src/button.c
@@ -7,8 +7,9 @@
 #include "util.h"
 #include "widgetprivate.h"
 
-DWidgetSignalReturn dButtonHandlerButtonPress(const DWidgetSignalEvent *event, void *userData);
-DWidgetSignalReturn dButtonHandlerButtonRelease(const DWidgetSignalEvent *event, void *userData);
+DWidgetSignalReturn dButtonHandlerWidgetButtonPress(const DWidgetSignalEvent *event, void *userData);
+DWidgetSignalReturn dButtonHandlerWidgetButtonRelease(const DWidgetSignalEvent *event, void *userData);
+DWidgetSignalReturn dButtonHandlerWidgetLeave(const DWidgetSignalEvent *event, void *userData);
 
 DWidget *dButtonNew(DWidget *child) {
 	// Create widget instance
@@ -32,14 +33,15 @@ void dButtonConstructor(DWidget *widget, DWidgetObjectData *data, DWidget *child
 	data->d.button.pressed=false;
 
 	// Connect signals to handle clicking logic
-	if (!dWidgetSignalConnect(widget, DWidgetSignalTypeWidgetButtonPress, &dButtonHandlerButtonPress, NULL) ||
-	    !dWidgetSignalConnect(widget, DWidgetSignalTypeWidgetButtonRelease, &dButtonHandlerButtonRelease, NULL)) {
+	if (!dWidgetSignalConnect(widget, DWidgetSignalTypeWidgetButtonPress, &dButtonHandlerWidgetButtonPress, NULL) ||
+	    !dWidgetSignalConnect(widget, DWidgetSignalTypeWidgetButtonRelease, &dButtonHandlerWidgetButtonRelease, NULL) ||
+	    !dWidgetSignalConnect(widget, DWidgetSignalTypeWidgetLeave, &dButtonHandlerWidgetLeave, NULL)) {
 		// This shouldn't really happen - there is no reason the handlers can fail to connect
 		dFatalError("error: could not connect internal signals for Button %p\n", widget);
 	}
 }
 
-DWidgetSignalReturn dButtonHandlerButtonPress(const DWidgetSignalEvent *event, void *userData) {
+DWidgetSignalReturn dButtonHandlerWidgetButtonPress(const DWidgetSignalEvent *event, void *userData) {
 	assert(event!=NULL);
 	assert(userData==NULL);
 
@@ -52,15 +54,37 @@ DWidgetSignalReturn dButtonHandlerButtonPress(const DWidgetSignalEvent *event, v
 	return DWidgetSignalReturnStop;
 }
 
-DWidgetSignalReturn dButtonHandlerButtonRelease(const DWidgetSignalEvent *event, void *userData) {
+DWidgetSignalReturn dButtonHandlerWidgetButtonRelease(const DWidgetSignalEvent *event, void *userData) {
 	assert(event!=NULL);
 	assert(userData==NULL);
 
 	DWidgetObjectData *data=dWidgetGetObjectDataNoFail(event->widget, DWidgetTypeButton);
 
-	// clear pressed flag
+	// Not pressed? (can happen if mouse is moved over button while held)
+	if (!data->d.button.pressed)
+		return DWidgetSignalReturnContinue;
+
+	// Clear pressed flag
 	data->d.button.pressed=false;
 
+	// Invoke button click signal
+	DWidgetSignalEvent dEvent;
+	dEvent.type=DWidgetSignalTypeButtonClick;
+	dEvent.widget=event->widget;
+	dWidgetSignalInvoke(&dEvent);
+
 	// Indicate we have handled this event
+	return DWidgetSignalReturnStop;
+}
+
+DWidgetSignalReturn dButtonHandlerWidgetLeave(const DWidgetSignalEvent *event, void *userData) {
+	assert(event!=NULL);
+	assert(userData==NULL);
+
+	DWidgetObjectData *data=dWidgetGetObjectDataNoFail(event->widget, DWidgetTypeButton);
+
+	// Clear pressed flag (but do not invoke clicked signal - consider process aborted)
+	data->d.button.pressed=false;
+
 	return DWidgetSignalReturnStop;
 }
